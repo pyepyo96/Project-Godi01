5a0910855e5cdd2b449cf165cab9ccac7b8f35fc
pyepyo96 <70050381+pyepyo96@users.noreply.github.com>
2024-12-03 00:14:59 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const axios = require('./node_modules/axios/index.d.cts');
+const axios = require('axios');
 require('dotenv').config();
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
